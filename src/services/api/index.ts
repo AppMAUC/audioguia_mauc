8e3b0ac92ac6885a54838d1b22e2f39631e3e048
Victor Emanuel Tomaz das Neves <victoremanuel.vet8@gmail.com>
2024-12-01 17:03:52 -0300

    axios config resolve

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // Substitua pela URL base da sua API
+  baseURL: import.meta.env.VITE_API_URL + "api", // Substitua pela URL base da sua API
   timeout: 10000, // Tempo limite de requisição em milissegundos
   headers: { "Content-Type": "application/json" },
 });
