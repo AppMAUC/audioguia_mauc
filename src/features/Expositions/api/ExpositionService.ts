b981704fa70d26a406a8737b2eef26bf5ba6cac3
Mateus de Aquino <86917791+mateusaquinomr@users.noreply.github.com>
2025-08-23 16:17:57 -0300

    paginação das páginas artistas e exposições

--- src/features/Expositions/api/ExpositionService.ts
+++ src/features/Expositions/api/ExpositionService.ts
@@ -5,6 +5,20 @@ export class ExpositionService extends apiService {
   constructor() {
     super(api, "/expositions");
   }
+
+  getAll<T>(page = 1, limit = 10): Promise<{
+    data: T[];
+    pages: number;
+    items: number;
+    next: number | null;
+    prev: number | null;
+    first: number;
+    last: number;
+  }> {
+    return this.api
+      .get(`${this.path}?page=${page}&limit=${limit}`)
+      .then((res) => res.data);
+  }
 }
 
 export default new ExpositionService();
